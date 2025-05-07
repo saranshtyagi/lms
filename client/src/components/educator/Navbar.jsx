c21e274924a3e070067885c4c1f9583e4069cbe3
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-05-07 22:59:14 +0530

    Minor spell-check

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       </Link>
       <div className='flex items-center gap-5 text-gray-500 relative'>
         <p>
-          Hi! {user ? user.fullName : 'Develeopers'}
+          Hi! {user ? user.fullName : 'Developers'}
         </p>
         {user ? <UserButton /> : <img className='max-w-8' src={assets.profile_img} />}
       </div>
